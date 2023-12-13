bf90b2247740b8e982990d08549b5b17dd1dbcc7
aNeuk95 <vkdlfl5dp@gmail.com>
2023-12-13 16:33:11 +0900

    굿즈 오류수정

--- project_team2/src/hex/trans/model/Trans.java
+++ project_team2/src/hex/trans/model/Trans.java
@@ -6,47 +6,41 @@ import java.util.Date;
 
 public class Trans {
 
-	private Integer trans_num;
-	private String item;
-	private Integer change_num;
-	private Date t_date;
-	// 여기서 지점을 등록뷰에서 보려면 지점도 넣어줘야 함
+    private Integer trans_num;
+    private String item;
+    private Integer change_num;
+    private Date t_date;
 
-	public Trans(Integer trans_num, String item, Integer change_num, Date t_date) {
-		this.trans_num = trans_num;
-		this.item = item;
-		this.change_num = change_num;
-		this.t_date = t_date;
-	}
+    public Trans(Integer trans_num, String item, Integer change_num, Date t_date) {
+        this.trans_num = trans_num;
+        this.item = item;
+        this.change_num = change_num;
+        this.t_date = t_date;
+    }
 
-	public Integer getTrans_num() {
-		return trans_num;
-	}
+    public Integer getTrans_num() {
+        return trans_num;
+    }
 
-	public String getItem() {
-		return item;
-	}
+    public String getItem() {
+        return item;
+    }
 
-	public Integer getChange_num() {
-		return change_num;
-	}
+    public Integer getChange_num() {
+        return change_num;
+    }
 
-	public Date getT_date() {
-		return t_date;
-	}
+    public Date getT_date() {
+        return t_date;
+    }
 
-	
-	
-	public String getFormattedT_date() {
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD");
-		return sdf.format(this.t_date);
-	}
+    public String getFormattedT_date() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(this.t_date);
+    }
 
-	public void setFormattedT_date(String formattedDate) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD");
-		this.t_date = sdf.parse(formattedDate);
-	}
-
-	
-	
+    public void setFormattedT_date(String formattedDate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        this.t_date = sdf.parse(formattedDate);
+    }
 }
